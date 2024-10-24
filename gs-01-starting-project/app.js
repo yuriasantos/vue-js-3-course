75ff8637c83dbb832d1269a257388adaa6025ade
yuri santos <santosyurial@gmail.com>
2024-10-24 13:44:59 -0300

    Start Vue.js 3 course.

---
+++ gs-01-starting-project/app.js
@@ -0,0 +1,16 @@
+const buttonEl = document.querySelector('button');
+const inputEl = document.querySelector('input');
+const listEl = document.querySelector('ul');
+
+function addGoal() {
+  const enteredValue = inputEl.value;
+  if (enteredValue.trim().length === 0) {
+    return;
+  }
+  const listItemEl = document.createElement('li');
+  listItemEl.textContent = enteredValue;
+  listEl.appendChild(listItemEl);
+  inputEl.value = '';
+}
+
+buttonEl.addEventListener('click', addGoal);
