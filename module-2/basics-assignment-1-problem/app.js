b37c0731092a7ffb6945e0307603ce6d05cff4ad
yuri santos <santosyurial@gmail.com>
2024-10-24 22:49:27 -0300

    Reorganize folders and practice interpolation, data binding, methods, outputting html and listening to events.

---
+++ module-2/basics-assignment-1-problem/app.js
@@ -0,0 +1,11 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      myName: 'Yuri',
+      myAge: 0,
+      imgUrl: 'https://s2-g1.glbimg.com/AFW86yTLGxM7fBVSmzLA1R_addw=/31x0:947x671/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2019/R/4/eFVj74TU6DffkgtAEDTg/14500529-1766823346903113-7499433856902780915-o.jpg'
+    };
+  }
+});
+
+app.mount('#assignment');
