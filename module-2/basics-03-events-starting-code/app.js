a83fd7e89770db6ab82911740c973baa0e097882
yuri santos <santosyurial@gmail.com>
2024-10-26 18:20:31 -0300

    Add event listeners on submit and keyup actions.

--- module-2/basics-03-events-starting-code/app.js
+++ module-2/basics-03-events-starting-code/app.js
@@ -3,6 +3,7 @@ const app = Vue.createApp({
     return {
       counter: 0,
       name: '',
+      confirmedName: '',
     };
   },
   methods: {
@@ -15,6 +16,12 @@ const app = Vue.createApp({
     setName(event) {
       this.name = event.target.value;
     },
+    submitForm() {
+      alert('Submitted!');
+    },
+    confirmInput() {
+      this.confirmedName = this.name;
+    }
   },
 });
 
