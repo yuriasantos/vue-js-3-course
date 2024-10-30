eff210fdc3852c7de7ca0cc9961ec7a9b48ccfee
yuri santos <santosyurial@gmail.com>
2024-10-29 21:38:06 -0300

    Add Vue lifecycle methods.

--- module-5/behind-scenes-01-starting-setup/app.js
+++ module-5/behind-scenes-01-starting-setup/app.js
@@ -10,6 +10,34 @@ const app = Vue.createApp({
       this.message = this.$refs.userInput.value;
     },
   },
+  beforeCreate() {
+    console.log('beforeCreate()');
+  },
+  created() {
+    console.log('created()');
+  },
+  beforeMount() {
+    console.log('beforeMount()');
+  },
+  mounted() {
+    console.log('mounted()');
+  },
+  beforeUpdate() {
+    console.log('beforeUpdate()');
+  },
+  updated() {
+    console.log('updated()');
+  },
+  beforeUnmount() {
+    console.log('beforeUnmount()');
+  },
+  unmounted() {
+    console.log('unmounted()');
+  },
 });
 
 app.mount('#app');
+
+setTimeout(() => {
+  app.unmount();
+}, 3000);
