5a632d2ae9d8c3720cf517fdce6dc97b999109df
yuri santos <santosyurial@gmail.com>
2024-11-03 11:11:46 -0300

    Practice slots, dynamic components and teleports in Vue.js.

---
+++ vue-project-4/src/components/UserInfo.vue
@@ -0,0 +1,20 @@
+<template>
+  <section>
+    <base-card>
+      <template #header>
+        <!-- # is the same as v-slot  -->
+        <h3>{{ fullName }}</h3>
+        <base-badge :type="role" :caption="role.toUpperCase()"></base-badge>
+      </template>
+      <template v-slot:default>
+        <p>{{ infoText }}</p>
+      </template>
+    </base-card>
+  </section>
+</template>
+
+<script>
+export default {
+  props: ['fullName', 'infoText', 'role'],
+}
+</script>
